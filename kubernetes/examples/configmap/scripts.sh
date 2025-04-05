c5675ef793fa38f01de454f4d610cb957629b1ec
Harsh Singh <harshkushwah2011@gmail.com>
2025-04-05 14:13:26 +0530

    done configmap

--- kubernetes/examples/configmap/scripts.sh
+++ kubernetes/examples/configmap/scripts.sh
@@ -1,2 +1,5 @@
 # 1. kubectl create configmap
 kubectl create configmap sport --from-literal=sport=football
+
+# 2. kubectl create another configmap
+kubectl create configmap sport --from-literal=fruit=apple
